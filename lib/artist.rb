dec501162a33ba45f89b73b93f7b37fbc4d292d8
Lizzie Chan <lchan217@gmail.com>
2019-03-11 13:15:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,17 +10,4 @@ class Artist
   def songs 
     @songs 
   end
-   def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-  def add_song_by_name(song)
-    new_song = Song.new(song)
-    new_song.artist = self 
-    @songs << new_song
-  end 
-  
-  def self.song_count 
-    Song.all.count
-  end 
 end
